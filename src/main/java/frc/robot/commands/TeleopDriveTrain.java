41421716304231237d18878876e2e868451f2339
ihernandez3647 <114842174+ihernandez3647@users.noreply.github.com>
2023-02-04 13:07:09 -0600

    Finished DriveTrain commands

---
+++ src/main/java/frc/robot/commands/TeleopDriveTrain.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Drivetrain;
+
+public class TeleopDriveTrain extends CommandBase {
+    /** Creates a new TeleopDriveTrain. */
+    Drivetrain drivetrain;
+    XboxController driver;
+
+    public TeleopDriveTrain(Drivetrain drivetrain, XboxController driver) {
+        // Use addRequirements() here to declare subsystem dependencies
+        this.drivetrain = drivetrain;
+        this.driver = driver;
+        addRequirements(drivetrain);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        drivetrain.driveTank(driver.getLeftY() * Constants.Motors.powerDecrease,
+            driver.getRightY() * Constants.Motors.powerDecrease);
+
+    }
+
+}
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,15 +4,16 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Drivetrain {
+public class Drivetrain extends SubsystemBase {
     CANSparkMax frontLeft;
     CANSparkMax frontRight;
     CANSparkMax backLeft;
     CANSparkMax backRight;
-    MotorControllerGroup left;
-    MotorControllerGroup right;
+    MotorControllerGroup leftGroup;
+    MotorControllerGroup rightGroup;
     DifferentialDrive tankDrive;
 
 
@@ -22,9 +23,14 @@ public class Drivetrain {
         frontRight = new CANSparkMax(Constants.Motors.FRONTRIGHTMOTORID, MotorType.kBrushless);
         backLeft = new CANSparkMax(Constants.Motors.BACKLEFTMOTORID, MotorType.kBrushless);
         backRight = new CANSparkMax(Constants.Motors.BACKRIGHTMOTORID, MotorType.kBrushless);
-        left = new MotorControllerGroup(frontLeft, backLeft);
-        right = new MotorControllerGroup(frontRight, backRight);
-        tankDrive = new DifferentialDrive(left, right);
+        leftGroup = new MotorControllerGroup(frontLeft, backLeft);
+        rightGroup = new MotorControllerGroup(frontRight, backRight);
+        tankDrive = new DifferentialDrive(leftGroup, rightGroup);
+        leftGroup.setInverted(true);
+    }
+
+    public void driveTank(double left, double right) {
+        tankDrive.tankDrive(left, right);
     }
 
 }
