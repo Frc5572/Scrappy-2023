73eabad59d1b4a26f39d55ca9fe7f2825644d056
ihernandez3647 <114842174+ihernandez3647@users.noreply.github.com>
2023-02-09 18:50:37 -0600

    Need to finish Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drivetrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -28,6 +29,7 @@ public class RobotContainer {
 
 
     /* Subsystems */
+    Drivetrain drivetrain = new Drivetrain();
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -45,7 +47,11 @@ public class RobotContainer {
      * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
      * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
-    private void configureButtonBindings() {}
+    private void configureButtonBindings() {
+        driver.getRightTriggerAxis();
+
+
+    }
 
     /**
      * Gets the user's selected autonomous command.
