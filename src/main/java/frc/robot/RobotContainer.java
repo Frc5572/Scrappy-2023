a5224681083e7bbdcaa8d062973d753630f84e2f
lcthehusky <96556378+lcthehusky@users.noreply.github.com>
2023-02-09 16:04:41 -0800

    Configured Buttons in Commands ligma

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.ArmMoving;
 import frc.robot.subsystems.Arm;
 
 /**
@@ -47,7 +48,12 @@ public class RobotContainer {
      * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
      * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
-    private void configureButtonBindings() {}
+    private void configureButtonBindings() {
+        operator.x().onTrue(new ArmMoving(arm1, Constants.ArmConstants.armPosHome));
+        operator.y().onTrue(new ArmMoving(arm1, Constants.ArmConstants.armPosFloor));
+        operator.b().onTrue(new ArmMoving(arm1, Constants.ArmConstants.armPosMiddle));
+        operator.a().onTrue(new ArmMoving(arm1, Constants.ArmConstants.armPosTop));
+    }
 
     /**
      * Gets the user's selected autonomous command.
